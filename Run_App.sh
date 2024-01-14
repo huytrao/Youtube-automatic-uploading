4d02885f4af33faaa0736326adb90cd959c58ea7
Huy Trao <121539558+huytrao@users.noreply.github.com>
2024-01-14 11:03:12 +0700

    Update Run_App.sh

--- Run_App.sh
+++ Run_App.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Get user input
-read -p "file name (ex: 1.mp4): " file
-read -p "title " title
-read -p "description: " description
-read -p "keywords: " keywords
+upload_more="yes"
 
-# Run the Python script
-python app.py --file="$file" --title="$title" --description="$description" --keywords="$keywords" --category="22" --privacyStatus="private"
+while [ "$upload_more" == "yes" ]; do
+    # Get user input
+    read -p "file name (ex: 1.mp4): " file
+    read -p "title: " title
+    read -p "description: " description
+    read -p "keywords: " keywords
+
+    # Run the Python script
+    python app.py --file="$file" --title="$title" --description="$description" --keywords="$keywords" --category="22" --privacyStatus="private"
+
+    # Ask if the user wants to upload more
+    read -p "Do you want to upload more? (yes/no): " upload_more
+done
