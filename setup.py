9341fa1a9abf534f644130c09255fbd42f5a68c7
Huy Trao <121539558+huytrao@users.noreply.github.com>
2024-01-18 07:06:45 +0700

    fix libr

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ os.system('sudo apt-get install python')
 os.system('sudo apt-get install python-pip')
 os.system('pip install --upgrade google-api-python-client')
 os.system('pip install --upgrade google-auth-oauthlib google-auth-httplib2')
-
+os.system('pip install oauth2client')
